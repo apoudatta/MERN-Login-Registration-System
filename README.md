249571428c46bad0639bae86afac1b42ba4f2e22
apoudatta <apoudatto6@gmail.com>
2024-11-08 12:26:08 +0600

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MERN-Login-Registration-System
+
+```bash
+    npm init -y
+    npm install express mongoose bcryptjs jsonwebtoken cors dotenv
+    npm install --save-dev nodemon
+```
---
+++ backend/routes/auth.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const router = express.Router();
+
+// Register user
+router.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(400).json({ message: 'User already exist' })
+        
+        const newUser = new User({ username, email, password });
+        await newUser.save();
+
+        const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(201).json({ token });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
